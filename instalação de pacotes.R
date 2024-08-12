b017c63a568f62ae264efaa01fdb7572670ab64c
VivianMelotto <vivian.v.melotto@gmail.com>
2024-08-12 15:11:29 -0300

    ajustes inclusão graficos no html

--- instalação de pacotes.R
+++
@@ -1,9 +0,0 @@
-#Passo 1: Instalar e Carregar os Pacotes Necessários
-# Instalar e carregar os pacotes necessários
-install.packages("dplyr")
-install.packages("ggplot2")
-install.packages("tidyr")
-
-library(dplyr)
-library(ggplot2)
-library(tidyr)
